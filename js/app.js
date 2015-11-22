eba22eece6343cbe647ac560647a0ea36ab11fd4
MiKolka <kolka.koval@yandex.ru>
2015-11-22 14:13:19 +0200

    add loading

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ angular.module('vstupApp', [
     , 'ui.router'
     , 'ngDialog'
     , 'LocalStorageModule'
-    //, 'ngAnimate'
+    , 'angular-loading-bar'
 
     //, 'vstupApp.run'
     //, 'vstupApp.config'
@@ -14,4 +14,34 @@ angular.module('vstupApp', [
     , 'vstupApp.ctrl.app'
     , 'vstupApp.ctrl.search'
     , 'vstupApp.ctrl.cabinet'
+    , 'vstupApp.ctrl.resultZno'
 ]);
+
+function uniqToArray(a, b) {
+    var arr = _.clone(a),
+        arrSelect = _.clone(b);
+
+    _.forEach(arr, function(arrItem, i){
+        _.forEach(arrSelect, function (item) {
+            if (arrItem && arrItem.name === item.name)
+                arr[i] = null;
+        });
+    });
+    return _.compact(arr);
+}
+
+function summ (arr) {
+    var s = 0;
+    var i = 0;
+
+    _.forEach(arr, function (item) {
+        if (item.active) {
+            i++;
+            return s += item.value;
+        }
+    });
+
+    s = s / i;
+
+    return '' + s.toFixed(2);
+}
