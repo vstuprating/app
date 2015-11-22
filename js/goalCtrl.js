1fddf328a900c2878bf003395f5a89755d3cffc6
MiKolka <kolka.koval@yandex.ru>
2015-11-22 15:43:20 +0200

    page goal, add secret function

--- js/goalCtrl.js
+++ js/goalCtrl.js
@@ -1,10 +1,23 @@
 
 angular.module('vstupApp.ctrl.goal', []).
-    controller('goalCtrl', function ($scope) {
+    controller('goalCtrl', function ($scope, $http) {
 
 
         $scope.setStep = function (name) {
             $scope.step = name;
+        };
+
+        $scope.setSpec = function (e) {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+                console.time('a')
+
+                $http.get('http://web.prolaby.com/api/get/directions', {params: {idBranch: 41}})
+                    .success(function (data) {
+                        $scope.v = true;
+                        console.timeEnd('a')
+                    });
+            }
         };
 
         $scope.map = [
